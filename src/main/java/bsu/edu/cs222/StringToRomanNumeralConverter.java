1ea3886696e94d41d21112e6781980c01920db8b
Reginald McCoy <rmccoy@bsu.edu>
2021-04-20 13:00:44 -0400

    Changed each ".replace" with ".replaceFirst", duplicate letters handled, all tests pass

--- src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
+++ src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
@@ -8,55 +8,55 @@ public class StringToRomanNumeralConverter {
 
         while (stringQuery.contains("CM")){
             finalInteger += 900;
-            stringQuery = stringQuery.replace("CM","");
+            stringQuery = stringQuery.replaceFirst("CM","");
         }
         while (stringQuery.contains("M")){
             finalInteger += 1000;
-            stringQuery = stringQuery.replace("M","");
+            stringQuery = stringQuery.replaceFirst("M","");
         }
         while (stringQuery.contains("CD")){
             finalInteger += 400;
-            stringQuery = stringQuery.replace("CD","");
+            stringQuery = stringQuery.replaceFirst("CD","");
         }
         while (stringQuery.contains("D")){
             finalInteger += 500;
-            stringQuery = stringQuery.replace("D","");
+            stringQuery = stringQuery.replaceFirst("D","");
         }
         while (stringQuery.contains("XC")){
             finalInteger += 90;
-            stringQuery = stringQuery.replace("XC","");
+            stringQuery = stringQuery.replaceFirst("XC","");
         }
         while (stringQuery.contains("C")){
             finalInteger += 100;
-            stringQuery = stringQuery.replace("C","");
+            stringQuery = stringQuery.replaceFirst("C","");
         }
         while (stringQuery.contains("XL")){
             finalInteger += 40;
-            stringQuery = stringQuery.replace("XL", "");
+            stringQuery = stringQuery.replaceFirst("XL", "");
         }
         while (stringQuery.contains("L")){
             finalInteger += 50;
-            stringQuery = stringQuery.replace("L", "");
+            stringQuery = stringQuery.replaceFirst("L", "");
         }
         while (stringQuery.contains("IX")){
             finalInteger += 9;
-            stringQuery = stringQuery.replace("IX","");
+            stringQuery = stringQuery.replaceFirst("IX","");
         }
         while (stringQuery.contains("X")){
             finalInteger += 10;
-            stringQuery = stringQuery.replace("X", "");
+            stringQuery = stringQuery.replaceFirst("X", "");
         }
         while (stringQuery.contains("IV")){
             finalInteger += 4;
-            stringQuery = stringQuery.replace("IV","");
+            stringQuery = stringQuery.replaceFirst("IV","");
         }
         while (stringQuery.contains("V")){
             finalInteger += 5;
-            stringQuery = stringQuery.replace("V","");
+            stringQuery = stringQuery.replaceFirst("V","");
         }
         while (stringQuery.contains("I")){
             finalInteger += 1;
-            stringQuery = stringQuery.replace("I","");
+            stringQuery = stringQuery.replaceFirst("I","");
         }
         return finalInteger;
     }
