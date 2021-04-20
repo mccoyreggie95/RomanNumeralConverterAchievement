acb79bdb5473e9dcb3f2d11eefdc3b1c57cf5ef2
Reginald McCoy <rmccoy@bsu.edu>
2021-04-20 12:33:26 -0400

    Changed all if statements to while loops

--- src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
+++ src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
@@ -6,27 +6,27 @@ public class StringToRomanNumeralConverter {
         stringQuery = stringQuery.trim();
         stringQuery = stringQuery.toUpperCase();
 
-        if (stringQuery.contains("M")){
+        while (stringQuery.contains("M")){
             finalInteger += 1000;
             stringQuery = stringQuery.replace("M","");
         }
-        if (stringQuery.contains("CM")){
+        while (stringQuery.contains("CM")){
             finalInteger += 900;
             stringQuery = stringQuery.replace("CM","");
         }
-        if (stringQuery.contains("D")){
+        while (stringQuery.contains("D")){
             finalInteger += 500;
             stringQuery = stringQuery.replace("D","");
         }
-        if (stringQuery.contains("CD")){
+        while (stringQuery.contains("CD")){
             finalInteger += 400;
             stringQuery = stringQuery.replace("CD","");
         }
-        if (stringQuery.contains("C")){
+        while (stringQuery.contains("C")){
             finalInteger += 100;
             stringQuery = stringQuery.replace("C","");
         }
-        if (stringQuery.contains("XC")){
+        while (stringQuery.contains("XC")){
             finalInteger += 90;
             stringQuery = stringQuery.replace("XC","");
         }
@@ -34,27 +34,27 @@ public class StringToRomanNumeralConverter {
             finalInteger += 50;
             stringQuery = stringQuery.replace("L", "");
         }
-        if (stringQuery.contains("XL")){
+        while (stringQuery.contains("XL")){
             finalInteger += 40;
             stringQuery = stringQuery.replace("XL", "");
         }
-        if (stringQuery.contains("X")){
+        while (stringQuery.contains("X")){
             finalInteger += 10;
             stringQuery = stringQuery.replace("X","");
         }
-        if (stringQuery.contains("IX")){
+        while (stringQuery.contains("IX")){
             finalInteger += 9;
             stringQuery = stringQuery.replace("IX", "");
         }
-        if (stringQuery.contains("V")){
+        while (stringQuery.contains("V")){
             finalInteger += 5;
             stringQuery = stringQuery.replace("V","");
         }
-        if (stringQuery.contains("IV")){
+        while (stringQuery.contains("IV")){
             finalInteger += 4;
             stringQuery = stringQuery.replace("IV","");
         }
-        if (stringQuery.contains("I")){
+        while (stringQuery.contains("I")){
             finalInteger += 1;
             stringQuery = stringQuery.replace("I","");
         }
