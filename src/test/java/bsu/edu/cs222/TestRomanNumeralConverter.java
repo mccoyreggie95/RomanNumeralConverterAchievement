620cc70861488dce05c82824386e804418b6f769
Reginald McCoy <rmccoy@bsu.edu>
2021-04-20 11:34:05 -0400

    Transformed test into parameterized test; some pass some fail

--- src/test/java/bsu/edu/cs222/TestRomanNumeralConverter.java
+++ src/test/java/bsu/edu/cs222/TestRomanNumeralConverter.java
@@ -2,13 +2,16 @@ package bsu.edu.cs222;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestRomanNumeralConverter {
     @Test
-    public void testToIntConverter() {
+    @ParameterizedTest
+    @CsvSource({"i,1","iv,4","v,5","cdxcix, 499"})
+    public void testToIntConverter(String query, int expected) {
         StringToRomanNumeralConverter converter = new StringToRomanNumeralConverter();
-        int expected = 11;
-        int actual = converter.convert("xi");
+        int actual = converter.convert(query);
         Assertions.assertEquals(expected, actual);
     }
 }
