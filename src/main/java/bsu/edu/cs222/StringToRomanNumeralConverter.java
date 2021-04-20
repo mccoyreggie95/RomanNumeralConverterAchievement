b5763df3eb148382902a64aed919f62aa4e541db
Reginald McCoy <rmccoy@bsu.edu>
2021-04-20 12:41:47 -0400

    Changed the order in which the code adds to the finalInteger.

--- src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
+++ src/main/java/bsu/edu/cs222/StringToRomanNumeralConverter.java
@@ -6,53 +6,53 @@ public class StringToRomanNumeralConverter {
         stringQuery = stringQuery.trim();
         stringQuery = stringQuery.toUpperCase();
 
-        while (stringQuery.contains("M")){
-            finalInteger += 1000;
-            stringQuery = stringQuery.replace("M","");
-        }
         while (stringQuery.contains("CM")){
             finalInteger += 900;
             stringQuery = stringQuery.replace("CM","");
         }
-        while (stringQuery.contains("D")){
-            finalInteger += 500;
-            stringQuery = stringQuery.replace("D","");
+        while (stringQuery.contains("M")){
+            finalInteger += 1000;
+            stringQuery = stringQuery.replace("M","");
         }
         while (stringQuery.contains("CD")){
             finalInteger += 400;
             stringQuery = stringQuery.replace("CD","");
         }
-        while (stringQuery.contains("C")){
-            finalInteger += 100;
-            stringQuery = stringQuery.replace("C","");
+        while (stringQuery.contains("D")){
+            finalInteger += 500;
+            stringQuery = stringQuery.replace("D","");
         }
         while (stringQuery.contains("XC")){
             finalInteger += 90;
             stringQuery = stringQuery.replace("XC","");
         }
-        while (stringQuery.contains("L")){
-            finalInteger += 50;
-            stringQuery = stringQuery.replace("L", "");
+        while (stringQuery.contains("C")){
+            finalInteger += 100;
+            stringQuery = stringQuery.replace("C","");
         }
         while (stringQuery.contains("XL")){
             finalInteger += 40;
             stringQuery = stringQuery.replace("XL", "");
         }
-        while (stringQuery.contains("X")){
-            finalInteger += 10;
-            stringQuery = stringQuery.replace("X","");
+        while (stringQuery.contains("L")){
+            finalInteger += 50;
+            stringQuery = stringQuery.replace("L", "");
         }
         while (stringQuery.contains("IX")){
             finalInteger += 9;
-            stringQuery = stringQuery.replace("IX", "");
+            stringQuery = stringQuery.replace("IX","");
         }
-        while (stringQuery.contains("V")){
-            finalInteger += 5;
-            stringQuery = stringQuery.replace("V","");
+        while (stringQuery.contains("X")){
+            finalInteger += 10;
+            stringQuery = stringQuery.replace("X", "");
         }
         while (stringQuery.contains("IV")){
             finalInteger += 4;
             stringQuery = stringQuery.replace("IV","");
+        }
+        while (stringQuery.contains("V")){
+            finalInteger += 5;
+            stringQuery = stringQuery.replace("V","");
         }
         while (stringQuery.contains("I")){
             finalInteger += 1;
@@ -61,5 +61,3 @@ public class StringToRomanNumeralConverter {
         return finalInteger;
     }
 }
-
-
